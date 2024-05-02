d467dfdf15c54b6266cb5a9a2fad621f7e800834
Candas Kat <hrisohest@gmail.com>
2024-05-02 15:16:21 +0200

    Add main function in test

--- tst/main.cpp
+++ tst/main.cpp
@@ -1,3 +1,10 @@
 //
 // Created by candas on 1/19/24.
 //
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
